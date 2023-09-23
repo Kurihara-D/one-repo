48c5cbc3b1c2c194a4adce587b1698840159fc48
Kurihara-D <kurihara@startup-technology.com>
2023-09-23 16:30:40 +0900

    fix build option

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,9 @@ RUN go mod download
 
 COPY . .
 EXPOSE 3000
+
+ENV GO111MODULE=on
+
 RUN go get -u github.com/cosmtrek/air && \
     go build -o /go/bin/air github.com/cosmtrek/air
 #CMD ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -6,11 +6,8 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋! ")
+		return c.SendString("Hello, World 👋!!!!!!")
 	})
 
-	err := app.Listen(":3000")
-	if err != nil {
-		return
-	}
+	app.Listen(":3000")
 }
